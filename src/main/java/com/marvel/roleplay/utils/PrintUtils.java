7f001c9421aa4b141cfc61faa7d85c412c8045b7
SahilChhabra <Sahil.Chhabra@target.com>
2018-10-06 20:43:08 +0530

    Refactored code

--- src/main/java/com/marvel/roleplay/utils/PrintUtils.java
+++ src/main/java/com/marvel/roleplay/utils/PrintUtils.java
@@ -4,12 +4,26 @@ import static com.marvel.roleplay.constants.RolePlayApiConstants.BOUNDARY_LINE;
 import static com.marvel.roleplay.constants.RolePlayApiConstants.CHARACTERS_LEFT_BORDER;
 import static com.marvel.roleplay.constants.RolePlayApiConstants.CHARACTERS_MIDDLE_BORDER;
 import static com.marvel.roleplay.constants.RolePlayApiConstants.CHARACTERS_RIGHT_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_CONTROLS_LINE;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_CONTROL_LEFT_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_CONTROL_MIDDLE_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_CONTROL_RIGHT_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_STATS_ENERGY_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_STATS_LEFT_EMPTY;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_STATS_LINE;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_STATS_MIDDLE_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_STATS_NAME_BORDER;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.GAME_STATS_RIGHT_BORDER;
 import static com.marvel.roleplay.constants.RolePlayApiConstants.LONG_BOUNDARY_LINE;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.PLAYER1;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.PLAYER2;
+import static com.marvel.roleplay.constants.RolePlayApiConstants.PLAYER_MOVES;
 
 import java.util.List;
 import java.util.Map;
 
 import com.marvel.roleplay.domains.MarvelCharacter;
+import com.marvel.roleplay.domains.Player;
 import com.marvel.roleplay.enums.FightAction;
 
 public class PrintUtils {
@@ -51,4 +65,44 @@ public class PrintUtils {
       Thread.sleep(500);
     }
   }
+
+  public static void printGameControls() {
+    println(GAME_CONTROLS_LINE);
+    println(GAME_CONTROL_LEFT_BORDER + String.format("%22s", "Exit Game") + GAME_CONTROL_MIDDLE_BORDER + String.format(
+        "%22s", "Enter X") + GAME_CONTROL_RIGHT_BORDER);
+    println(GAME_CONTROL_LEFT_BORDER + String.format("%22s", "Save & Exit Game") + GAME_CONTROL_MIDDLE_BORDER
+        + String.format("%22s", "Enter A") + GAME_CONTROL_RIGHT_BORDER);
+    println(PLAYER_MOVES);
+    println(
+        GAME_CONTROL_LEFT_BORDER + String.format("%22s", FightAction.PUNCH.name()) + GAME_CONTROL_MIDDLE_BORDER + String
+            .format("%22s", "Enter P") + GAME_CONTROL_RIGHT_BORDER);
+    println(
+        GAME_CONTROL_LEFT_BORDER + String.format("%22s", FightAction.SUPER_PUNCH.name()) + GAME_CONTROL_MIDDLE_BORDER
+            + String.format("%22s", "Enter S") + GAME_CONTROL_RIGHT_BORDER);
+    println(GAME_CONTROL_LEFT_BORDER + String.format("%22s", FightAction.FRONT_KICK.name()) + GAME_CONTROL_MIDDLE_BORDER
+        + String.format("%22s", "Enter K") + GAME_CONTROL_RIGHT_BORDER);
+    println(
+        GAME_CONTROL_LEFT_BORDER + String.format("%22s", FightAction.FLYING_KICK.name()) + GAME_CONTROL_MIDDLE_BORDER
+            + String.format("%22s", "Enter F") + GAME_CONTROL_RIGHT_BORDER);
+    println(GAME_CONTROL_LEFT_BORDER + String.format("%22s", FightAction.FLIP.name()) + GAME_CONTROL_MIDDLE_BORDER
+        + String.format("%22s", "Enter L") + GAME_CONTROL_RIGHT_BORDER);
+    println(GAME_CONTROL_LEFT_BORDER + String.format("%22s", FightAction.BLOCK.name() + " (Default Move)")
+        + GAME_CONTROL_MIDDLE_BORDER + String.format("%22s", "Enter B or Any other") + GAME_CONTROL_RIGHT_BORDER);
+    println(BOUNDARY_LINE);
+  }
+
+  public static void printGameStats(Player player1, Player player2) {
+    println(GAME_STATS_LINE);
+    println(GAME_STATS_LEFT_EMPTY + String.format("%20s", PLAYER1) + GAME_STATS_MIDDLE_BORDER + String.format("%24s",
+        PLAYER2) + GAME_STATS_RIGHT_BORDER);
+    println(BOUNDARY_LINE);
+    println(GAME_STATS_NAME_BORDER + String.format("%20s", player1.getCharacter().getName()) + GAME_STATS_MIDDLE_BORDER
+        + String.format("%24s", player2.getCharacter().getName()) + GAME_STATS_RIGHT_BORDER);
+    println(GAME_STATS_ENERGY_BORDER + String.format("%20s", player1.getEnergyLevel()) + GAME_STATS_MIDDLE_BORDER
+        + String.format("%24s", player2.getEnergyLevel()) + GAME_STATS_RIGHT_BORDER);
+    printWithBoundary("Next Turn : " + (player1.getHasNextTurn() ?
+        player1.getCharacter().getName() :
+        player2.getCharacter().getName()));
+  }
+
 }
